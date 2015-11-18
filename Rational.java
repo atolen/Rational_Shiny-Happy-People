0505c8a6a1a8dbb7ece942cdbdd52ec59fb96009
anna tolen <atolen@stuy.edu>
2015-11-18 14:48:12 -0500

    fixed stuff

--- Rational.java
+++ Rational.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Team Angry Inch -- Anna Tolen
 APCS1 pd10
 HW33 -- Do You Even Add, Bro?
@@ -13,33 +12,7 @@ public class Rational {
     public Rational() {
 	setN( 0 );
 	setD( 1 );
-=======
-/* Team Shiny Happy People -- Shamaul Dilmohamed, Anna Tolen
-APCS1 pd10
-HW32 -- Irrationality Stops Here
-2015-11-17 */
-
-public class Rational {
-    // instvars
-    private int numerator;
-    private int denominator;
-
-    // constructors
-    public Rational() {
-	setNumerator( 0 );
-	setDenominator( 1 );
->>>>>>> 43eeb7414121ec5bbf65193b448c28b1e7343ea7
     }
-
-    public Rational( int n, int d ) {
-	this();
-	if( d != 0 ) {
-<<<<<<< HEAD
-	    setN( n );
-	    setD( d );
-	}
-    } // close Rational(n,d)
-  
     // mutators
     public void setN( int newN ) {
 	n = newN;
@@ -47,52 +20,20 @@ public class Rational {
 
     public void setD( int newD ) {
 	d = newD;
-=======
-	    setNumerator( n );
-	    setDenominator( d );
-	}
-    } // close Rational(n,d)
-
-    // accessors
-    public int getNumerator() {
-	return numerator;
-    }
-
-    public int getDenominator() {
-	return denominator;
-    }
-    
-    // mutators
-    public void setNumerator( int n ) {
-	numerator = n;
-    }
-
-    public void setDenominator( int d ) {
-	denominator = d;
->>>>>>> 43eeb7414121ec5bbf65193b448c28b1e7343ea7
     }
 
     // other methods
     public String toString() {
-<<<<<<< HEAD
 	String retStr = n + "/" + d;
-=======
-	String retStr = numerator + "/" + denominator;
->>>>>>> 43eeb7414121ec5bbf65193b448c28b1e7343ea7
 	return retStr;
     }
 
     public double floatValue() {
-<<<<<<< HEAD
 	double retDub = (double)n / d;
-=======
-	double retDub = (double)numerator / denominator;
->>>>>>> 43eeb7414121ec5bbf65193b448c28b1e7343ea7
 	return retDub;
     }
     
     public void multiply( Rational r ) {
-<<<<<<< HEAD
 	int n = this.n * r.n;
 	int d = this.d * r.d;
 	this.setN( n );
@@ -129,28 +70,11 @@ public class Rational {
 
     
     public void reduce() {
-	int n = this.n;
-	int d = this.d;
-	
 	int gcd = gcd( Math.max(n,d) , Math.min(n,d) );
 	setN( n / gcd );
 	setD( d / gcd );
-=======
-	int n = this.numerator * r.getNumerator();
-	int d = this.denominator * r.getDenominator();
-	this.setNumerator( n );
-	this.setDenominator( d );
     }
-    
-	
-    public void divide( Rational r ) {
-       	int n = this.numerator * r.denominator;
-	int d = this.denominator * r.numerator;
-	this.setNumerator( n );
-	this.setDenominator( d );
->>>>>>> 43eeb7414121ec5bbf65193b448c28b1e7343ea7
-    }
-    
+        
 } // close class Rational
 
 	    
